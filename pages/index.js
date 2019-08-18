533f3eb039887be1d833b29b5de6985043c214bf
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-08-17 19:50:07 -0700

    🖋 Set title for landing page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import {
   About,
   Contact,
@@ -11,6 +13,11 @@ import {
 const Landing = () => {
   return (
     <>
+      <Head>
+        <title>Wilhelm Willie | code + design + product</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
       <Hero />
       <Dope />
       <LineBreak />
